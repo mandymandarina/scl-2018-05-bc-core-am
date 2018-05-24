cdf23fd77858384b8dc515d8dd8604e317477b9c
Miranda Naranjo <m.naranjo.medel@gmail.com>
2018-05-24 18:36:48 -0400

    se le integro una estructura al HTML

--- src/index.js
+++ src/index.js
@@ -1,1 +1,12 @@
 /* Acá va tu código */
+window.onload = function(){
+    var btn = document.getElementById("enter-one");
+    var btntwo= document.getElementById("code");
+    var btnt3= document.getElementById("decode");
+
+       
+
+    btnEmail.addEventListener('click',function(){
+    var email=document.getElementById("codigo").value;
+    document.getElementById("codigo").innerHTML="<p>tu codigo codificado es </p>" + ;
+       })
