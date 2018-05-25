7c9cb34f964cfaacf892e6db98e53fbf0be83248
Miranda Naranjo <m.naranjo.medel@gmail.com>
2018-05-25 11:55:03 -0400

    se agrego estilo en el css para los botones

--- src/index.js
+++ src/index.js
@@ -1,12 +1,24 @@
 /* Acá va tu código */
-window.onload = function(){
-    var btn = document.getElementById("enter-one");
-    var btntwo= document.getElementById("code");
-    var btnt3= document.getElementById("decode");
+window.onload =()=> {
+    const btn = document.getElementById("code");
+    const btntwo= document.getElementById("decode");
+    const btnthree=document.getElementById("thkx");
 
        
 
-    btnEmail.addEventListener('click',function(){
-    var email=document.getElementById("codigo").value;
-    document.getElementById("codigo").innerHTML="<p>tu codigo codificado es </p>" + ;
-       })
+    btn.addEventListener("click",function() {
+    message=document.getElementById("enter-one").value;
+      document.getElementById("message").innerHTML="<h3>Tu mensaje codificado es:</h3><div><input type='text' id='enter-two' value=''></div> ";
+    })
+
+    btntwo.addEventListener("click", function() {
+    messagetwo=document.getElementById("enter-three").value;
+      document.getElementById("messagetwo").innerHTML="<h3>Tu mensaje decodificado es:</h3><div><input type='text' id='enter-four' value=''></div> ";
+    })
+    
+    btnthree.addEventListener("click", function() {
+    messagethree=document.getElementById("commentary").value;
+      document.getElementById("messagethree").innerHTML= messagethree;
+    })
+
+}
