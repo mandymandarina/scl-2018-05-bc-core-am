2705262b9f8e9d197a529fd84b30d9469e4d0bb4
Miranda Naranjo <m.naranjo.medel@gmail.com>
2018-05-31 08:40:00 -0400

    arregle el enlace de .enncode

--- src/cipher.js
+++ src/cipher.js
@@ -1,16 +1,17 @@
 window.cipher = {
     encode: (text, offset)=> {
       let result ="";
+      let textCipher ="";
       for (let i=0; i<text.length; i++) {
-        var textChar = ((text.charCodeAt(i) - 65 + 33)%26) + 65;
-        var textCipher = String.fromCharCode(textChar);
+        var textChar = ((text.charCodeAt(i) - 65 + offset)%26) + 65;
+        textCipher += String.fromCharCode(textChar);
       }
        return textCipher;
   },
   decode: (text, offset) => {
     let result ="";
     for (let i=0; i<text.length; i++){
-      var textChar = ((text.charCodeAt(i) - 65 - 33)%26) + 65;
+      var textChar = ((text.charCodeAt(i) - 65 - offset)%26) + 65;
       var textDecipher = String.fromCharCode(textChar);
     }
   return textDecipher;
--- src/index.js
+++ src/index.js
@@ -1,20 +1,19 @@
 /* Acá va tu código */
 /*para cifrar */
-window.onload =()=> {
+window.onload = () => {
 
- const btn = document.getElementById("code");
+  const btn = document.getElementById("code");
 
-  btn.addEventListener("click", function() {
-  let offsetNum = document.getElementById("number-one").value;
-  const enterText = document.getElementById("enter-one").value;
-  const outText = window.ciper.encode(enterText, offsetNum);
-  document.getElementById("enter-two").innerHTML =outText;
-})
-  
- const btnThree = document.getElementById("thkx");
+  btn.addEventListener("click", function () {
+    let offsetNum = document.getElementById("number-one").value;
+    const enterText = document.getElementById("enter-one").value;
+    document.getElementById("enter-two").innerHTML = window.cipher.encode(enterText, offsetNum);
+  })
 
-  btnThree.addEventListener("click", function() {
-  messagethree=document.getElementById("commentary").value;
-  document.getElementById("messagethree").innerHTML= messagethree;
-})
+  const btnThree = document.getElementById("thkx");
+
+  btnThree.addEventListener("click", function () {
+    messagethree = document.getElementById("commentary").value;
+    document.getElementById("messagethree").innerHTML = messagethree;
+  })
 }
